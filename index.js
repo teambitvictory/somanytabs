684c86b147617f22fa044dab9be10ba066c05790
Stefan Blamberg <stefan.blamberg@sap.com>
2021-04-05 12:53:43 +0200

    Get user permission before opening bookmark

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const awake = () => {
+const byId = document.getElementById.bind(document);
+const show = (id, shouldShow = true) => { byId(id).style.display = (shouldShow ? 'block' : 'none'); };
+
+const getOpenTabsCallback = (tabs) => () => {
   if (document.body.getAttribute('extension-available')) {
     // Extension takes over, no further handling required
+    document.body.dispatchEvent(new Event('OPEN_TABS'));
     return;
   }
-
-  const session = (new URLSearchParams(window.location.search)).get('session');
-  const { tabs } = JSON.parse(decodeURIComponent(session));
 
   const newTabs = tabs.map((url) => window.open(url));
 
@@ -13,8 +14,31 @@ const awake = () => {
     !newTab || newTab.closed || newTab.closed == null
   ))) {
     // Popups blocked
-    // TODO
-    console.error('Popups are being blocked');
+    show('permission');
+  }
+  show('extension');
+};
+
+const awake = () => {
+  const session = (new URLSearchParams(window.location.search)).get('session');
+  const { tabs } = JSON.parse(decodeURIComponent(session));
+  const openTabs = getOpenTabsCallback(tabs);
+
+  const allowedBookmarks = JSON.parse(localStorage.getItem('smt.allowedBookmarks') || '[]');
+  if (allowedBookmarks.some((bookmark) => bookmark === tabs.join('; '))) {
+    openTabs();
+  } else {
+    // Ask for permission first
+    byId('confirmation-url-list').innerHTML = tabs.map((tab) => `<li>${tab}</li>`).join('');
+    byId('confirmation-btn').addEventListener('click', async () => {
+      localStorage.setItem(
+        'smt.allowedBookmarks',
+        JSON.stringify([...allowedBookmarks, tabs.join('; ')]),
+      );
+      show('confirmation', false);
+      openTabs();
+    });
+    show('confirmation');
   }
 };
 
